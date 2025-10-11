b08bb0195f6f91dbcd29bf1e7d78946f9e72a7e1
shahriar00 <shahriarsazid783@gmail.com>
2025-10-11 11:32:34 +0600

    Initial Commit

---
+++ AnimationEverything/AnimationEverythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationEverythingApp.swift
+//  AnimationEverything
+//
+//  Created by Shahriar Islam Sazid on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationEverythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationEverythingTests/AnimationEverythingTests.swift
@@ -0,0 +1,16 @@
+//
+//  AnimationEverythingTests.swift
+//  AnimationEverythingTests
+//
+//  Created by Shahriar Islam Sazid on 10/11/25.
+//
+
+import Testing
+
+struct AnimationEverythingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
